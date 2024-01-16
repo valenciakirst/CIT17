cba565cc5af1b35a5881a4b3297c01e614d6bf91
valenciakirst <candykaye87@gmail.com>
2024-01-16 22:26:37 +0800

    new lesson 4

---
+++ Doubles.php
@@ -0,0 +1,23 @@
+<?php
+
+print "DOUBLES";
+
+//This is practice for doubles
+
+$many = 2.3333;
+$many_2 = 2.4444;
+$few = $many + $many_2;
+
+print "<br><br>2.3334 + 2.4444 = $few <br>";
+
+print "<br> Solve for the Value of Pi: <br>
+        <br> A boy walks around a circle which has a diameter of 100m.
+        <br> How far has the boy walked?";
+
+$pi = 3.141592653589793238462643383279502884197;
+$distance = 100;
+
+$circumference = $pi * $distance;
+
+print "<br><br> The distance walked is the circumference, therefore pi($pi) multiplied to the distance walked ($distance) is equal to $circumference.";
+
