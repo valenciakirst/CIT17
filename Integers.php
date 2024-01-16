0dc15b63efe9a7df041482a47364c01628b0ef2f
valenciakirst <candykaye87@gmail.com>
2024-01-16 22:07:10 +0800

    new lesson 3

---
+++ Integers.php
@@ -0,0 +1,21 @@
+<?php
+
+//integers
+
+$int_var = 2003;
+$another_int = 777 - 666;
+$new_int = 555 + 445;
+
+print "I was born in $int_var <br>";
+print "If you minus 777 to 666, you get $another_int <br>";
+print "If you add 555 to 445, your sum will be $new_int <br>";
+print "<br>";
+
+if ($int_var > $another_int) {
+    print "yahoo <br>";
+}
+if ($new_int < $int_var){
+    print "lahlah";
+}
+
+?>
---
+++ blocks.php
@@ -0,0 +1,16 @@
+<?php
+
+//braces make blocks practice
+
+if (5 != 2 + 1)
+    print("Good - I definitely lost my mind. <br>");
+
+
+if (3 == 2 + 1) {
+    print ("Good - I haven't totally <br>");
+    print ("lost my mind. <br>");
+}
+
+
+
+?>
