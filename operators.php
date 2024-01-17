59fb5e543a0e47b861ee68a9d0c7084a133fb557
valenciakirst <candykaye87@gmail.com>
2024-01-17 22:42:09 +0800

    Lesson 6

---
+++ operators.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operators</title>
+</head>
+<body>
+    <?php 
+
+    print "Lesson 6: Operator Types"
+    $a =42;
+    $b = 20;
+
+    $c = $a + $b;
+    echo "Addition Oberation Result $c <br>";
+    $c = $a - $b;
+    echo "Subtraction Oberation Result $c <br>";
+    $c = $a * $b;
+    echo "Multiplication Oberation Result $c <br>";
+    $c = $a / $b;
+    echo "Division Oberation Result $c <br>";
+    $c = $a % $b;
+    echo "Modulus Oberation Result $c <br>";
+    $c = $a++;
+    echo "Increment Oberation Result $c <br>";
+    $c = $a--;
+    echo "Decrement Oberation Result $c <br>"; 
+    
+    ?>
+
+    
+    
+</body>
+</html>
