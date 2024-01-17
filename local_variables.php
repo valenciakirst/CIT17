bde4d0ed4c28a8419cfacd180e7ba0aace52b0c1
valenciakirst <candykaye87@gmail.com>
2024-01-17 21:37:25 +0800

    Lesson 4.7

---
+++ local_variables.php
@@ -0,0 +1,15 @@
+<?php
+print "Lesson 4.7<br><br>";
+
+
+$x = 4;
+function assignx () {
+    $x = 0;
+    print "\$x inside function is $x. <br>";
+
+}
+
+assignx();
+print "\$x outside of fuction is x";
+
+?>
